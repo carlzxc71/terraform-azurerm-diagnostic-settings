b575e83ac8fe92bfabb1bc4404b16d9320e7fdf3
Carl Lindberg <carlzxc71@gmail.com>
2025-12-16 12:45:32 +0100

    Add module readme, add variable description and outputs

--- variables.tf
+++ variables.tf
@@ -1,40 +1,50 @@
 variable "environment" {
-  type = string
+  type        = string
+  description = "The environment used for the deployment"
 }
 
 variable "location" {
-  type = string
+  type        = string
+  description = "The location used for the deployment"
 }
 
 variable "location_short" {
-  type = string
+  type        = string
+  description = "The short form of the location used for the deployment"
 }
 
 variable "common_name" {
-  type = string
+  type        = string
+  description = "The common name used for the deployment"
 }
 
 variable "retention_in_days" {
-  type = number
+  type        = number
+  description = "The number of days to retain logs"
 }
 
 variable "resource_name" {
-  type = string
+  type        = string
+  description = "The name of the resource"
 }
 
 variable "target_resource_id" {
-  type = string
+  type        = string
+  description = "Target resource ID of the resource where the diagnostic setting is created"
 }
 
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "The name of the resource group"
 }
 
 variable "deploy_log_analytics_workspace" {
-  type = bool
+  type        = bool
+  description = "Enable or disable the creation of a log analytics workspace with this module"
 }
 
 variable "log_analytics_workspace_resource_id" {
-  type    = string
-  default = null
+  type        = string
+  default     = null
+  description = "The resource ID of an external workspace"
 }
